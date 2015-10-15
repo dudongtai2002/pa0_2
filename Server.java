bc8c1ffb72ccf175fade8c4968aec31487c50bbe
dudongtai2002 <dudongtai@wireless1x-155-41-97-16.bu.edu>
2015-10-14 22:27:02 -0400

    Save1

--- Server.java
+++ Server.java
@@ -4,11 +4,40 @@
  * and open the template in the editor.
  */
 package pa0_2;
-
+import java.util.*;
 /**
  *
  * @author dudongtai
  */
 public class Server {
+    double Rate=1;
+    long time;
+    ArrayList<LinkedList<Packet>> queue=new ArrayList <>();
+    ArrayList<Event> timeline=new ArrayList<> ();
+    int total_packet;
+    long total_bits;
+    long total_delay;
+    public Server(){
+        time=0;
+        for(int i=0;i<11;i++){
+            LinkedList<Packet> a=new LinkedList<>();
+            queue.add(a);
+            }
+        total_packet=0;
+        total_bits=0;
+        total_delay=0;
+     
+    }
+    //put the first 11 packet in the timeline
+    public void init(){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
